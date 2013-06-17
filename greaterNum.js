5dd83a21983989c002522495074983d16443072d
timestep <jbosshellokitty@gmail.com>
2013-06-17 12:54:05 -0400

    greater number comparitor

---
+++ greaterNum.js
@@ -0,0 +1,14 @@
+function greaterNum(x,y){
+	if x>y{
+		return x;
+		console.log('The greater number of '+x+'and '+y+'is'+x)
+	}
+	else {
+		return y;
+		console.log('The greater number of '+x+'and '+y+'is'+y)
+	}
+}
+
+greaterNum(1,3);
+greaterNum(4,2);
+
