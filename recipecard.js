fd8d28f30e0082031be01608028711431f71d40e
timestep <jbosshellokitty@gmail.com>
2013-06-17 13:48:36 -0400

    recipee cards

---
+++ recipecard.js
@@ -0,0 +1,9 @@
+var recipe = {
+	title: "number1";
+	servings: 1;
+	ingredients: ['awesome','power','man']; 
+}
+
+console.log(recipe.title);
+console.log(recipe.servings);
+console.log(recipe.ingredients) // i guess you can write a for loop but im lazy. ;\
