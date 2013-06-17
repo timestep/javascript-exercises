20b0d315f332f805e9d40123a7f137bf4a092325
timestep <jbosshellokitty@gmail.com>
2013-06-17 12:58:41 -0400

    helloworld

---
+++ world.js
@@ -0,0 +1,18 @@
+function helloWorld('lang'){
+	if lang == 'fr'{
+		return "Bonjour tout la monde";
+	}
+	else if lang == 'es'{
+		return "Hola Mundo";
+	}
+	else if lang == 'br'{
+		return "Ola Mundo";
+	}
+	else 
+		return 'Hello World';
+}
+
+console.log(helloWorld('fr'))
+console.log(helloWorld('es'))
+console.log(helloWorld('br'))
+console.log(helloWorld('en'))
