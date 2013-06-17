7d67e45cd848b1d9ae00edb9c70d3cf217e2cd4a
timestep <jbosshellokitty@gmail.com>
2013-06-17 12:22:56 -0400

    age calculator

---
+++ agecalculator.js
@@ -0,0 +1,11 @@
+var currentYear = new Date();
+currentYear = currentYear.getFullYear();
+var birthYear = 1990
+
+(function(){
+	age = currentYear - birthYear;
+	age2 = age - 1;
+	console.log('You are either' + age + 'or' + age2)
+})()
+
+
--- fortuneteller.js
+++ fortuneteller.js
@@ -1,8 +1,8 @@
-var numOfChildren = 3
-var partnerName = 'hehe'
-var geographicLocation = 'somewhere hot'
-var jobTitle = 'none'
+var numOfChildren = 3 ;
+var partnerName = 'hehe' ;
+var geographicLocation = 'somewhere hot';
+var jobTitle = 'none';
 
 (function(){
-	console.log('You will be a' + jobTitle + 'in' + geographicLocation + ',and married to' + partnerName + 'with' + numOfChildren + 'kids.')		
+	console.log('You will be a' + jobTitle + 'in' + geographicLocation + ',and married to' + partnerName + 'with' + numOfChildren + 'kids.');		
 })()
