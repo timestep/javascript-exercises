e66184f4b6ca011ab4afa06503febeec53d5d620
timestep <jbosshellokitty@gmail.com>
2013-06-17 12:36:31 -0400

    refactoring age calculator

--- agecalculator.js
+++ agecalculator.js
@@ -1,11 +1,13 @@
-var currentYear = new Date();
-currentYear = currentYear.getFullYear();
-var birthYear = 1990;
 
-(function(){
+function calculateAge(birthYear){
+	var currentYear = new Date();
+	currentYear = currentYear.getFullYear();
 	age = currentYear - birthYear;
 	age2 = age - 1;
 	console.log('You are either' + age + 'or' + age2);
 })()
 
+calculateAge(1990);
+calculateAge(2001);
+calculateAge(900);
 
