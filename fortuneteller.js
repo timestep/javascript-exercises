1c988c1234dd50d71bdfda7c0b54ee3a9d909c34
timestep <jbosshellokitty@gmail.com>
2013-06-17 12:15:16 -0400

    inital commit + fortuneteller exercise

---
+++ fortuneteller.js
@@ -0,0 +1,8 @@
+var numOfChildren = 3
+var partnerName = 'hehe'
+var geographicLocation = 'somewhere hot'
+var jobTitle = 'none'
+
+(function(){
+	console.log('You will be a' + jobTitle + 'in' + geographicLocation + ',and married to' + partnerName + 'with' + numOfChildren + 'kids.')		
+})()
