c2107f9af55a3a77948a1c33e0e9ba2262acc7bc
timestep <jbosshellokitty@gmail.com>
2013-06-17 12:38:24 -0400

    refactorng lifetime supply calculator

--- lifetime.js
+++ lifetime.js
@@ -1,8 +1,10 @@
-var currentAge;
-var maxAge;
-var estimatePerDay;
+var maxAge = 100;
 
-(function(){
-	total = (maxAge-currentAge)*365*estimatePerDay;
+function calculateSupply(age, estimatePerDay){
+	total = (maxAge-age)*365*estimatePerDay;
 	console.log('You will need'+total+'to last you until the ripe old age of'+maxAge);
-})()
+})
+
+calculateSupply(19,200);
+calculateSupply(20,300);
+calculateSupply(50,20);
