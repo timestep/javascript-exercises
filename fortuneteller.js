00c4a61ca881943ee57c85f7f7108817eabc3d40
timestep <jbosshellokitty@gmail.com>
2013-06-17 12:34:58 -0400

    refactoring fortune teller

--- fortuneteller.js
+++ fortuneteller.js
@@ -1,8 +1,7 @@
-var numOfChildren = 3 ;
-var partnerName = 'hehe' ;
-var geographicLocation = 'somewhere hot';
-var jobTitle = 'none';
-
-(function(){
+function tellFortune(numOfChildren, partnerName, geographicLocation, jobTitle){
 	console.log('You will be a' + jobTitle + 'in' + geographicLocation + ',and married to' + partnerName + 'with' + numOfChildren + 'kids.');		
-})()
+}
+
+tellFortune(3,'job','maruitis','job');
+tellFortune(9,'michelle','costarica','non');
+tellFortune(1,'jon','toronto','none');
